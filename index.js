77fb7e96f8b214db69027411b639aeff41a4dcf9
Benjamin ABEL <bbig26@gmail.com>
2016-01-23 11:48:37 +0100

    Lint with eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('./node_modules/google-closure-library/closure/goog/bootstrap/nodejs');
 
-const fs = require('fs')
+const fs = require('fs');
 const SmilesParser = require('./src/io/smiles_parser');
 const CoordinateGenerator = require('./src/layout/coordinate_generator');
 const SvgDepict = require('./src/depict/svg_depict');
 
 //console.log(SmilesParser);
-const smiles = 'N[C@@H](CCC(=O)N[C@@H](CSSC[C@H](NC(=O)CC[C@H](N)C(O)=O)C(=O)NCC(O)=O)C(=O)NCC(O)=O)C(O)=O';
+const smiles = 'N[C@@H](CCC(=O)N[C@@H](CSSC[C@H](NC(=O)CC[C@H](N)C(O)=O)C(=O)NCC(O)=O)C(=O)NCC(O)=O)C(O)=O'; // eslint-disable-line
 //const smiles = 'CCCC(O)=O';
 //const smiles = 'O=C=O';
 var mol = SmilesParser.parse(smiles);
@@ -14,12 +14,13 @@ var mol = SmilesParser.parse(smiles);
 
 CoordinateGenerator.generate(mol);
 
-var svg = SvgDepict.toSvg(100, 100, mol)
+var svg = SvgDepict.toSvg(100, 100, mol);
 
-fs.writeFile("/home/ben/Bureau/mol.svg", svg, function(err) {
-    if(err) {
-        return console.log(err);
-    }
+// write svg file
+fs.writeFile('/home/ben/Bureau/mol.svg', svg, function(err) {
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log("Le svg est enregistré sur le Bureau");
+  console.log('Le svg est enregistré sur le Bureau');
 });
