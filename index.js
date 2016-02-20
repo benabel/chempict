355d0112755678ac07e8e399167c720de748677b
Benjamin ABEL <bbig26@gmail.com>
2016-02-20 18:29:37 +0100

    Rename svg_depict file -> depict/svg.js

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ require('./node_modules/google-closure-library/closure/goog/bootstrap/nodejs');
 const fs = require('fs');
 const SmilesParser = require('./src/io/smiles_parser');
 const CoordinateGenerator = require('./src/layout/coordinate_generator');
-const SvgDepict = require('./src/depict/svg_depict');
+const SvgDepict = require('./src/depict/svg');
 
 //console.log(SmilesParser);
 // const smiles = 'N[C@@H](CCC(=O)N[C@@H](CSSC[C@H](NC(=O)CC[C@H](N)C(O)=O)C(=O)NCC(O)=O)C(=O)NCC(O)=O)C(O)=O'; // eslint-disable-line
-// TODO: probleme avec les cycles
 //const smiles = 'O=C(O)c1ccccc1OC(=O)C';
 // aspirine
-const smiles = 'CC(=O)OC1=CC=CC=C1C(=O)O';
+// const smiles = 'CC(=O)OC1=CC=CC=C1C(=O)O';
+// lactose
+const smiles = 'C([C@@H]1[C@@H]([C@@H]([C@H]([C@@H](O1)O[C@@H]2[C@H](O[C@H]([C@@H]([C@H]2O)O)O)CO)O)O)O)O';
 //const smiles = 'CCCC(O)=O';
-//const smiles = 'O=C=O';
+// const smiles = 'O=C=O';
 var mol = SmilesParser.parse(smiles);
 //console.log(mol.atoms);
 
